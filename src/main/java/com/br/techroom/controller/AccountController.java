cd5597fa23d5300d146929530eaeae8fb777407d
Victor Vilar <victor.svilar@gmail.com>
2023-09-30 12:46:48 -0300

    created the 'api/v1/login' endpoint to manage the login operation.

--- src/main/java/com/br/techroom/controller/AccountController.java
+++ src/main/java/com/br/techroom/controller/AccountController.java
@@ -1,4 +1,21 @@
 package com.br.techroom.controller;
 
+import com.br.techroom.dto.responses.LoginResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class AccountController {
+
+
+    @GetMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(){
+
+        return ResponseEntity.ok().build();
+    }
+
+
 }
