112674c06a42db425997ba6c484a15a281a6244d
Victor Vilar <victor.svilar@gmail.com>
2023-09-30 16:47:37 -0300

    addeed spring validation to LoginRequestDto class.

--- src/main/java/com/br/techroom/dto/requests/LoginRequestDTO.java
+++ src/main/java/com/br/techroom/dto/requests/LoginRequestDTO.java
@@ -2,10 +2,14 @@ package com.br.techroom.dto.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class LoginRequestDTO {
 
+    @NotNull(message="Must inform the username or password")
     private String username;
+    @NotNull(message="Must inform the password")
     private String password;
 
 }
