6fca42170d18cd99d8b41dc87934db008b9ff93a
Victor Vilar <victor.svilar@gmail.com>
2023-09-30 16:27:47 -0300

    create LoginDto to use as an object to store data from a logim attempt.

--- src/main/java/com/br/techroom/dto/requests/LoginRequestDTO.java
+++ src/main/java/com/br/techroom/dto/requests/LoginRequestDTO.java
@@ -1,2 +1,11 @@
-package com.br.techroom.dto.requests;public class LoginRequestDTO {
+package com.br.techroom.dto.requests;
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+
+    private String username;
+    private String password;
+
 }
