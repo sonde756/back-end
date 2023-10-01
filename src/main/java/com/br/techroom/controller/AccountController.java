9b3f2e98af4ad5094255c400a5aa26076d45c183
Victor Vilar <victor.svilar@gmail.com>
2023-10-01 12:54:59 -0300

    implemented the attemptAuthentication method inside AccountController to do the authentication process.

--- src/main/java/com/br/techroom/controller/AccountController.java
+++ src/main/java/com/br/techroom/controller/AccountController.java
@@ -1,8 +1,13 @@
 package com.br.techroom.controller;
 
+import com.br.techroom.dto.requests.LoginRequestDTO;
 import com.br.techroom.dto.responses.LoginResponseDTO;
+import com.br.techroom.service.impl.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class AccountController {
 
+    @Autowired
+    private AccountServiceImpl accountService;
 
     @GetMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(){
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<LoginResponseDTO> attemptAuthentication(@RequestBody LoginRequestDTO loginRequestDTO){
+        return  ResponseEntity.ok(this.accountService.attemptAuthentication(loginRequestDTO));
     }
 
 
