d990c90ee312a354981270cca3518e2bf64aaf9c
Victor Vilar <victor.svilar@gmail.com>
2023-10-01 12:56:31 -0300

    AccountModel implments UserDetails interface and its methods.

--- src/main/java/com/br/techroom/model/AccountModel.java
+++ src/main/java/com/br/techroom/model/AccountModel.java
@@ -1,4 +1,70 @@
 package com.br.techroom.model;
 
-public class AccountModel {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="accounts")
+public class AccountModel implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_id")
+    private Integer idd;
+
+    private String username;
+    private String email;
+    @Column(name="user_password")
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        //TODO
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        //TODO
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //TODO
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //TODO
+        return true;
+    }
 }
