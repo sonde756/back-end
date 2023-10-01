52c79071b5b6aed8c09a63f6ed95799fe596d97d
Victor Vilar <victor.svilar@gmail.com>
2023-10-01 12:59:29 -0300

    created JwtValidatorFilter to get the token if exist from the authentication header of the request. It will filter all endoints but /login and /register.

--- src/main/java/com/br/techroom/config/JwtValidatorFilter.java
+++ src/main/java/com/br/techroom/config/JwtValidatorFilter.java
@@ -1,2 +1,75 @@
-package com.br.techroom.config;public class JwtValidatorFilter {
+package com.br.techroom.config;
+
+import com.br.techroom.service.impl.JwtService;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * Filter to get the token and validate. This filter is placed before the basicAuthenticationFilter.
+ * If the request url it is the login enpoint this filter will not filter.
+ */
+public class JwtValidatorFilter extends OncePerRequestFilter {
+
+    private static final String AUTHENTICATION_HEADER = "Authentication";
+    private static final String BEARER_HEADER = "Bearer ";
+
+
+    private JwtService jwtService;
+
+    @Autowired
+    public JwtValidatorFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //getting the Authenticaton Header from request
+        String jwt = request.getHeader(AUTHENTICATION_HEADER);
+
+        // if the token is null then the filterchain do filter and return
+        if(jwt == null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //if the jwt does not start with 'Bearer ' then filterchain do filer and return
+        if(!jwt.startsWith(BEARER_HEADER)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //replace 'Bearer ' for ''
+        jwt = jwt.replace(BEARER_HEADER,"");
+
+        //try to validate the jwt,
+        Claims claims = this.jwtService.validateToken(jwt);
+
+        //if the token is valid then creates a new Authentication
+        Authentication auth = new UsernamePasswordAuthenticationToken(claims.get("username"),null,null);
+
+        //putting the Authentication inside security context holder.
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getRequestURL().equals("/api/v1/login") || request.getRequestURL().equals("/api/v1/register");
+    }
+
 }
