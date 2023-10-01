c147d50221758a284de75c33cc68261302848321
Victor Vilar <victor.svilar@gmail.com>
2023-10-01 12:55:35 -0300

    create methods to find de account by username and by email.

--- src/main/java/com/br/techroom/repository/AccountRepository.java
+++ src/main/java/com/br/techroom/repository/AccountRepository.java
@@ -4,6 +4,11 @@ import com.br.techroom.model.AccountModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<AccountModel,Long> {
+
+    Optional<AccountModel> findByUsername(String username);
+    Optional<AccountModel> findByEmail(String email);
 }
