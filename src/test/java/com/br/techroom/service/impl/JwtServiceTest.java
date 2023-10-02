62285b2eff913f6e21a7ad791344e09ba876ea3b
Victor Vilar <victor.svilar@gmail.com>
2023-10-02 19:00:46 -0300

    todo:jwtServiceTest.

--- src/test/java/com/br/techroom/service/impl/JwtServiceTest.java
+++ src/test/java/com/br/techroom/service/impl/JwtServiceTest.java
@@ -1,24 +1,29 @@
 package com.br.techroom.service.impl;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * test for the JwtService class
+ * @author Victor Vilar
+ */
 class JwtServiceTest {
 
+    String token;
+
+    @BeforeEach
+    void setUp(){
+        token = "sjlksa0gjgjgjfg04g9jbklsj04-.8398740fsadf9a8s4thv.92387098";
+    }
+
     @Test
-    void generateToken() {
+    void generateTokenSuccesfully() {
     }
 
     @Test
     void validateToken() {
     }
 
-    @Test
-    void testGenerateToken() {
-    }
-
-    @Test
-    void testValidateToken() {
-    }
 }
