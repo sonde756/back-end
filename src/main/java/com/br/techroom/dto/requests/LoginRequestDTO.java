3a68499efc84dd5df7b50178cf7d6f68e142dd3c
Victor Vilar <victor.svilar@gmail.com>
2023-10-02 13:00:28 -0300

    fixed validation text.

--- src/main/java/com/br/techroom/dto/requests/LoginRequestDTO.java
+++ src/main/java/com/br/techroom/dto/requests/LoginRequestDTO.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotNull;
 @Data
 public class LoginRequestDTO {
 
-    @NotNull(message="Must inform the username or password")
+    @NotNull(message="Must inform the username or email")
     private String username;
     @NotNull(message="Must inform the password")
     private String password;
