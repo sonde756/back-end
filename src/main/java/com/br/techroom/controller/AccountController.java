2ae914af8d6278e0fe1a60a8e7e46ea525ffacef
sonde756 <edsonrafael756@gmail.com>
2023-10-04 09:54:58 -0300

    doc

--- src/main/java/com/br/techroom/controller/AccountController.java
+++ src/main/java/com/br/techroom/controller/AccountController.java
@@ -1,8 +1,10 @@
 package com.br.techroom.controller;
 
 
+import com.br.techroom.dto.requests.LoginRequestDTO;
 import com.br.techroom.dto.requests.RegisterRequestDTO;
 import com.br.techroom.dto.responses.ApiResponseRegister;
+import com.br.techroom.dto.responses.LoginResponseDTO;
 import com.br.techroom.dto.responses.RegisterResponseDTO;
 import com.br.techroom.service.AccountService;
 import io.swagger.annotations.Api;
@@ -17,18 +19,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import com.br.techroom.dto.requests.LoginRequestDTO;
-import com.br.techroom.dto.responses.LoginResponseDTO;
-import com.br.techroom.service.impl.AccountServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 /**
  * Classe responsável por controlar as requisições de registro de usuário.
@@ -41,11 +31,14 @@ import javax.validation.Valid;
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class AccountController {
-    @Autowired
-    private AccountService accountService;
+    private final AccountService accountService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    private ModelMapper modelMapper;
+    public AccountController(AccountService accountService, ModelMapper modelMapper) {
+        this.accountService = accountService;
+        this.modelMapper = modelMapper;
+    }
 
 
     /**
@@ -73,7 +66,9 @@ public class AccountController {
      * @param loginRequestDTO the username and the password
      * @return response entity of LoginResponseDto with its username and token
      */
-    @GetMapping("/login")
+    @ApiOperation(value = "Account login")
+    @ApiResponses(@ApiResponse(code = 200, message = "Account successfully logged in"))
+    @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> attemptAuthentication(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
         return  ResponseEntity.ok(this.accountService.attemptAuthentication(loginRequestDTO));
     }
