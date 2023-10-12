bc09126c695f0c8db7ab8a71481ddbf1e18af107
sonde756 <edsonrafael756@gmail.com>
2023-10-11 23:49:20 -0300

    fix

--- src/main/java/com/br/techroom/service/StatusService.java
+++ src/main/java/com/br/techroom/service/StatusService.java
@@ -1,10 +1,6 @@
 package com.br.techroom.service;
 
-import com.br.techroom.exception.ValidationRegisterException;
 import com.br.techroom.model.Status;
-import com.br.techroom.repository.StatusRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 
 public interface StatusService {
