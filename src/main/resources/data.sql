39688864118162c2aa74df84642441dfe79b2b8c
sonde756 <edsonrafael756@gmail.com>
2023-10-13 00:49:40 -0300

    leia

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ CREATE TABLE status (
                         id_status SERIAL PRIMARY KEY,
                         status VARCHAR(255) NOT NULL,
                         type_status VARCHAR(255) NOT NULL
-);
+); /*para roda fora do compose apague o create table*/
 
 INSERT INTO status (id_status, status, type_status) VALUES (1, 'New', 'type_user');
 INSERT INTO status (id_status, status, type_status) VALUES (2, 'Active', 'type_user');
